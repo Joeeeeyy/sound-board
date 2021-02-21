2cbb5d281c7f28216cbe44178bd316a4f9eb996a
Jose Lopez <joseglopez1289@gmail.com>
2021-02-21 07:54:57 -0800

    styled

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-let audio = document.getElementById('audio');
-
-playAudio = () => {
-    audio.play();
-};
+ $(document).ready(function () {
+     function play(str) {
+         var audio = document.getElementById(str);
+         audio.play();
+     }
+     $('#shh').on("click", function (e) {
+        $('#display').text('🤫');
+         play('F');
+     });
+     $('#moan').on("click", function (e) {
+        $('#display').text('👄');
+         play('G');
+     });
+    $('#fart').on("click", function (e) {
+        $('#display').text('💩');
+        play('H');
+    });
+    $('#moo').on("click", function (e) {
+        $('#display').text('🐮');
+        play('J');
+    });
+    
+    $(this).on("keydown", function (e) {
+        if (e.keyCode === 70) {
+            play('F');
+            $('#shh').fadeTo(100, 0.1).fadeTo(200, 1.0);
+            $('#display').text('🤫');
+        } else if (e.keyCode === 71) {
+            play('G');
+            $('#moan').fadeTo(100, 0.1).fadeTo(200, 1.0);
+            $('#display').text('👄');
+        } else if (e.keyCode === 72) {
+            play('H');
+            $('#fart').fadeTo(100, 0.1).fadeTo(200, 1.0);
+            $('#display').text('💩');
+        } else if (e.keyCode === 74) {
+            play('J');
+            $('#moo').fadeTo(100, 0.1).fadeTo(200, 1.0);
+            $('#display').text('🐮');
+        };
+    });
+    
+ });
